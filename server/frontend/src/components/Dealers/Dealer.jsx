a0974c70cd8576c2ca0d77dcb33df1d40b2c3937
Anandu <anandu.shivakumar@gmail.com>
2026-05-02 09:47:51 -0400

    Added Dynamic Pages, Dealer Details, view reviews, add review

--- server/frontend/src/components/Dealers/Dealer.jsx
+++ server/frontend/src/components/Dealers/Dealer.jsx
@@ -34,6 +34,7 @@ const Dealer = () => {
       let dealerobjs = Array.from(retobj.dealer)
       setDealer(dealerobjs[0])
     }
+    console.log("Dealer API response:", retobj);
   }
 
   const get_reviews = async ()=>{
@@ -71,14 +72,14 @@ return(
   <div style={{margin:"20px"}}>
       <Header/>
       <div style={{marginTop:"10px"}}>
-      <h1 style={{color:"grey"}}>{dealer.full_name}{postReview}</h1>
-      <h4  style={{color:"grey"}}>{dealer['city']},{dealer['address']}, Zip - {dealer['zip']}, {dealer['state']} </h4>
+      <h1 style={{color:"grey"}}>{dealer?.full_name}{postReview}</h1>
+      <h4  style={{color:"grey"}}>{dealer?.city},{dealer?.address}, Zip - {dealer?.zip}, {dealer?.state} </h4>
       </div>
       <div class="reviews_panel">
       {reviews.length === 0 && unreviewed === false ? (
         <text>Loading Reviews....</text>
       ):  unreviewed === true? <div>No reviews yet! </div> :
-      reviews.map(review => (
+      reviews && reviews.map(review => (
         <div className='review_panel'>
           <img src={senti_icon(review.sentiment)} className="emotion_icon" alt='Sentiment'/>
           <div className='review'>{review.review}</div>
